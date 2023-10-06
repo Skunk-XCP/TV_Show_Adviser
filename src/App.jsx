7efc5750074bb8f1e0726c24e7d1674cd64961b3
Skunk-XCP <skunk-xcp@hotmail.fr>
2023-10-06 18:09:30 +0200

    API request, display popular show, display rating

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,43 @@
+import { useEffect, useState } from "react";
+import { TVShowAPI } from "./api/tv-show";
+import "./global.css";
+import s from "./style.module.css";
+import { BACKDROP_BASE_URL } from "./config";
+import { TVShowDetail } from "./components/TVShowDetail/TVShowDetail";
+
 export function App() {
-    return <div>Hello</div>
+    const [currentTVShow, setCurrentTVShow] = useState();
+
+    async function fetchPopulars() {
+        const populars = await TVShowAPI.fetchPopulars();
+        if (populars.length > 0) {
+            setCurrentTVShow(populars[0])
+        }
+    }
+
+    useEffect(() => {
+        fetchPopulars();
+    }, []);
+
+    return (
+        <div className={s.main_container}
+            style={{ background: currentTVShow ? `linear-gradient(rgba(0,0,0,0.55), rgba(0,0,0,0.55)), url("${BACKDROP_BASE_URL}${currentTVShow.backdrop_path}") no-repeat center / cover` : "black", }}>
+            <div className={s.header}>
+                <div className="row">
+                    <div className="col-4">
+                        <div>Logo</div>
+                        <div>Subtitle</div>
+                    </div>
+                    <div className="col-sm-12 col-md-4">
+                        <input style={{ width: "100%" }} type="text" />
+                    </div>
+                </div>
+            </div>
+            <div className={s.tv_show_details}>
+                {currentTVShow && <TVShowDetail tvShow={currentTVShow} />}
+            </div>
+            <div className={s.recommanded_shows}>Recommanded tv shows</div>
+        </div>
+    )
 }
+
